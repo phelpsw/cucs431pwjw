4d80efc0476c1c9276a5228fe567eb8c996a7ece
phelpsw <phelpsw@872b8ea3-6c49-0410-8074-63f643fbca3b>
2008-04-29 02:38:24 +0000

    A simple java uri query example using aws.

---
+++ trunk/project2/AWS_OWL_Mashup/src/aws_owl/AwsHandler.java
@@ -0,0 +1,62 @@
+package aws_owl;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+public class AwsHandler {
+
+	public static int BOOK = 0;
+	public static int DVD = 1;
+	public static int MUSIC = 2;
+	
+	public AwsHandler()
+	{
+	}
+	
+	public void query(String agent, int type)
+	{
+		String url;
+		if(type == BOOK) {
+			url = "http://ecs.amazonaws.com/onca/xml?Service=AWSECommerceService&Operation=ItemSearch&AWSAccessKeyId=0RBFG1JEYFQRVTPWYQ02&SearchIndex=Books&Author="+forURL(agent);
+		} else if (type == DVD) { 
+			url = "http://ecs.amazonaws.com/onca/xml?Service=AWSECommerceService&Operation=ItemSearch&AWSAccessKeyId=0RBFG1JEYFQRVTPWYQ02&SearchIndex=DVD&Actor="+forURL(agent);
+		} else if (type == MUSIC) {
+			url = "http://ecs.amazonaws.com/onca/xml?Service=AWSECommerceService&Operation=ItemSearch&AWSAccessKeyId=0RBFG1JEYFQRVTPWYQ02&SearchIndex=Music&Artist="+forURL(agent);
+		} else {
+			// unknown
+			return;
+		}
+		
+		try {
+			
+			URL datafile = new URL(url);
+			URLConnection conn = datafile.openConnection();
+			
+			InputStreamReader isr = new InputStreamReader(conn.getInputStream());
+			BufferedReader reader = new BufferedReader(isr);
+			String str;
+			while ((str = reader.readLine()) != null) {
+				System.out.println(str);
+			}
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	private String forURL(String frag)
+	{
+		String result = null;
+		try 
+		{
+			result = URLEncoder.encode(frag, "UTF-8");
+	    } catch (UnsupportedEncodingException ex) {
+	    	throw new RuntimeException("UTF-8 not supported", ex);
+	    }
+		return result;
+	}
+}
+
---
+++ trunk/project2/AWS_OWL_Mashup/src/aws_owl/QueryTool.java
@@ -0,0 +1,18 @@
+package aws_owl;
+
+public class QueryTool {
+
+		
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		AwsHandler ah = new AwsHandler();
+		
+		ah.query("Nu:tone", AwsHandler.MUSIC);
+		
+	}
+
+}
