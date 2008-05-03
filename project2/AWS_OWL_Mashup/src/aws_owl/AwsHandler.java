abeb737151bd6dcb2c85e179b233b31effc044d9
justin.wruz <justin.wruz@872b8ea3-6c49-0410-8074-63f643fbca3b>
2008-05-03 23:21:44 +0000

    loading works for cd, dvd, and books

--- project2/AWS_OWL_Mashup/src/aws_owl/AwsHandler.java
+++ project2/AWS_OWL_Mashup/src/aws_owl/AwsHandler.java
@@ -1,7 +1,11 @@
 package aws_owl;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -16,18 +20,27 @@ import org.w3c.dom.*;
 
 import org.xml.sax.SAXException;
 
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.ontology.ProfileRegistry;
+import com.hp.hpl.jena.rdf.model.*;
+import com.hp.hpl.jena.vocabulary.*;
+
+
 public class AwsHandler {
 
 	public final static int BOOK = 0;
 	public final static int DVD = 1;
 	public final static int MUSIC = 2;
 	
+	public static int type;
 	public AwsHandler()
 	{
 	}
 	
 	public Document query(String agent, int type)
 	{
+		this.type = type;
 		String url;
 		if(type == BOOK) {
 			url = "http://ecs.amazonaws.com/onca/xml?Service=AWSECommerceService&ResponseGroup=Large&Operation=ItemSearch&AWSAccessKeyId=0RBFG1JEYFQRVTPWYQ02&SearchIndex=Books&Author="+forURL(agent);
@@ -43,15 +56,32 @@ public class AwsHandler {
 		try {
 			
 			URL datafile = new URL(url);
-			/*
+			
 			URLConnection conn = datafile.openConnection();
 			InputStreamReader isr = new InputStreamReader(conn.getInputStream());
 			BufferedReader reader = new BufferedReader(isr);
 			String str;
 			while ((str = reader.readLine()) != null) {
-				System.out.println(str+"\n");
+				//System.out.println(str+"\n");
 			}
-			*/
+			
+			File n3 = new File("../pathway.n3");
+			FileReader fr = new FileReader(n3);
+			
+			Model model = ModelFactory.createDefaultModel();
+			model.read(fr, null, "N3");
+			Resource guy = model.createResource("NSguy");
+			Property likes = model.createProperty("propURI","likes");
+			Resource girl = model.createResource("NSgirl");
+			//guy.addProperty(likes, model.createLiteral("girl"));
+			StmtIterator iter = model.listStatements();
+			Statement stmt;
+			while (iter.hasNext())
+		    {
+		    stmt = (Statement)iter.next();
+		    //System.out.println(stmt.getString());
+		    }
+			
 			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder builder = factory.newDocumentBuilder();
 			return builder.parse(url);
--- project2/AWS_OWL_Mashup/src/aws_owl/TripleLoader.java
+++ project2/AWS_OWL_Mashup/src/aws_owl/TripleLoader.java
@@ -1,16 +1,28 @@
 package aws_owl;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.Iterator;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+
+import org.mindswap.pellet.jena.PelletReasonerFactory;
 import org.w3c.dom.*;
 
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.ontology.OntModelSpec;
 import com.hp.hpl.jena.ontology.ProfileRegistry;
 import com.hp.hpl.jena.rdf.model.*;
+import com.hp.hpl.jena.reasoner.Reasoner;
+import com.hp.hpl.jena.reasoner.ValidityReport;
 import com.hp.hpl.jena.vocabulary.*;
 
 import org.xml.sax.SAXException;
@@ -19,59 +31,156 @@ public class TripleLoader {
 
 	Document xml;
 	int type;
+	static String aws = "http://www.owl-ontologies.com/Ontology1209425965.owl#";
+	Resource subj;
+	Property isWrittenBy;
+	Property isPerformedBy;
+	Property isPerformerIn;
+	Property isAuthorOf;
+	Property hasTitle;
+	Model model;
 	
-	static String isWrittenBy = "";
-	static String isPerformedBy = "";
-	static String isAuthorOf = "";
-	static String isPerformerOf = "";
-
-	
-	
-	public TripleLoader(Document xml, int type)
+	public TripleLoader(Document xml)
 	{
 		this.xml = xml;
-		this.type = type;
+		this.type = AwsHandler.type;
+		model = ModelFactory.createDefaultModel();
+		isWrittenBy = model.createProperty(aws, "isWrittenBy");
+		isPerformedBy = model.createProperty(aws, "isPerformedBy");
+		isPerformerIn = model.createProperty(aws, "isPerformerIn");
+		isAuthorOf = model.createProperty(aws,"isAuthorOf");
+		hasTitle = model.createProperty(aws,"hasTitle");
 	}
 	
 	public void Load()
 	{
-		Node items = xml.getElementsByTagName("Items").item(0);
 
+		Node items = xml.getElementsByTagName("Items").item(0);
 		if(items.getFirstChild().getFirstChild().getTextContent().equals("True"))
 		{
 			NodeList itemList = xml.getElementsByTagName("Item");
+			NodeList itemAttributesList = xml.getElementsByTagName("ItemAttributes");
+			NodeList Titles = xml.getElementsByTagName("Title");
 			for(int i=0; i<itemList.getLength(); i++)
-				nodeToTriple(itemList.item(i));
+				nodeToTriple((Element)itemList.item(i),(Element)itemAttributesList.item(i),
+						(Element)Titles.item(i));
 		}
 		else (new Exception("Invalid search query")).printStackTrace();
+		
+		writeToFile();
+		Inferencer();
 	}
 	
-	private void nodeToTriple(Node n)
+	private void nodeToTriple(Element item, Element itemAttributes, Element Title)
 	{
-		NodeList list = n.getChildNodes();
-		Element itemAttributes = null;
-		for(int i=0; i<list.getLength(); i++)
-		{
-			Node  node= list.item(i);
-			if(node.getNodeName()=="ItemAttributes") itemAttributes = (Element)node;
-		}
-		
-		Model model = ModelFactory.createDefaultModel();
-		Resource subj = model.createResource(itemAttributes.getFirstChild().getTextContent());
-		OntModel m = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM, model);
-		Property pisWrittenBy = model.createProperty("", isWrittenBy);
-		
+		String ASIN = item.getFirstChild().getTextContent();
+		Resource[] subjects = addAgents(itemAttributes, ASIN, type);
+		String title = Title.getTextContent();
+		Resource product = model.createResource(aws+ASIN);
+		String productCategory = "";
+		String agentCategory = "";
+		Property p = null;
 		
 		switch(type)
 		{
 		case AwsHandler.BOOK:
+			productCategory = "book";
+			agentCategory = "Author";
+			p = isAuthorOf;
 			break;
 		case AwsHandler.DVD:
+			productCategory = "DVD";
+			agentCategory = "Actor";
+			p = isPerformerIn;
 			break;
 		case AwsHandler.MUSIC:
+			productCategory = "CD";
+			agentCategory = "Artist";
+			p = isPerformerIn;
 			break;
 		}
 		
+		model.add(product, RDF.type, aws+productCategory);
+		model.add(product, hasTitle, model.createLiteral(title));
+		for(int i=0; i<subjects.length; i++)
+		{
+			model.add(subjects[i], p, product);
+			model.add(subjects[i], RDF.type, aws+agentCategory);
+		}
+	}
+	
+	private Resource[] addAgents(Element itemAttributes, String ASIN, int type)
+	{
+		String agentTag = itemAttributes.getFirstChild().getNodeName();
+		Element index = (Element)itemAttributes.getFirstChild();
+		//subj = model.createResource(aws+"Agent:"+agent);
+		int count = 0;
+		while(agentTag == index.getTagName())
+		{
+			index = (Element)index.getNextSibling();
+			count++;
+		}
+		
+		Resource[] subjects = new Resource[count];
+		index = (Element)itemAttributes.getFirstChild();
+		for(int i=0; i<count; i++)
+		{
+			subjects[i] = model.createResource(aws+"Agent:"+index.getTextContent());
+			index = (Element)index.getNextSibling();
+		}
+		return subjects;
+	}
+	
+	private void writeToFile()
+	{
+		try{
+			OutputStream out = new FileOutputStream("out.xml");
+			RDFWriter writer = model.getWriter("N3");
+			writer.setProperty("showXmlDeclaration","true");
+		    writer.setProperty("tab","8");
+		    writer.setProperty("relativeURIs","same-document,relative");
+			writer.write(model, out, null);
+			out.close();
+			}
+			catch(Exception e){}
+	}
+	
+	private void Inferencer()
+	{
+		File owl = new File("../aws_proj2.owl");
+		FileReader fr = null;
+		OntModel ontmodel;
+		try{
+		fr = new FileReader(owl);
+		ontmodel = ModelFactory.createOntologyModel( PelletReasonerFactory.THE_SPEC );
+		ontmodel.read(fr, null);
+		ValidityReport report = ontmodel.validate();
+        printIterator( report.getReports(), "Validation Results" );
+		/*
+		OutputStream out = new FileOutputStream("ontout.xml");
+		RDFWriter writer = ontmodel.getWriter("RDF/XML");
+		writer.setProperty("showXmlDeclaration","true");
+	    writer.setProperty("tab","8");
+	    writer.setProperty("relativeURIs","same-document,relative");
+		writer.write(ontmodel, out, null);
+		out.close();*/
+		}
+		catch(Exception e){}
 		
 	}
+	public static void printIterator(Iterator i, String header) {
+        System.out.println(header);
+        for(int c = 0; c < header.length(); c++)
+            System.out.print("=");
+        System.out.println();
+        
+        if(i.hasNext()) {
+	        while (i.hasNext()) 
+	            System.out.println( i.next() );
+        }       
+        else
+            System.out.println("<EMPTY>");
+        
+        System.out.println();
+    }
 }
