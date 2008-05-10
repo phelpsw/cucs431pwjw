c6f7389e0ebc5409463281290403d879ecde6d22
phelpsw <phelpsw@872b8ea3-6c49-0410-8074-63f643fbca3b>
2008-05-10 03:06:21 +0000

    Simple search interface up and running, next step is to break search off into separate thread, sparql queries.

---
+++ trunk/project2/nb_aws_owl/src/aws_owl/AWSSearchType.java
@@ -0,0 +1,49 @@
+/*
+ * AWSSearchType.java
+ *
+ * Created on May 9, 2008, 10:54 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package aws_owl;
+import java.lang.String;
+/**
+ *
+ * @author phelps
+ */
+public class AWSSearchType
+{
+    private int type;
+    
+    public final static int BOOK = 0;
+    public final static int DVD = 1;
+    public final static int MUSIC = 2;
+    
+    public AWSSearchType(int type)
+    {
+        this.type = type;
+    }
+    
+    public AWSSearchType()
+    {
+    }
+    
+    public int getType()
+    {
+        return type;
+    }
+    
+    public String toString()
+    {
+        if(type == BOOK)
+            return "Book";
+        else if(type == DVD)
+            return "DVD";
+        else if(type == MUSIC)
+            return "Music";
+        else
+            return "Unknown";
+    }
+}
